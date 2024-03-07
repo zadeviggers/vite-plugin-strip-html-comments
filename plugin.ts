f5088487b7dd740ef395a3a02433af8567b05ebb
Zade Viggers <zade@viggers.net>
2024-03-07 20:38:48 +1300

    Make JSR happy

--- plugin.ts
+++ plugin.ts
@@ -1,4 +1,7 @@
-export function stripHTMLComments() {
+export function stripHTMLComments(): {
+  name: string;
+  transformIndexHtml: (html: string) => string;
+} {
   return {
     name: "vite-plugin-strip-html-comments",
     transformIndexHtml(html) {
